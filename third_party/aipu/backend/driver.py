36b71468693c7a29e6d232b4d62e18cf1dc31858
arozha01 <aron.zhang@armchina.com>
2025-01-17 15:20:21 +0800

    [AIPU][CP-14800] Add demo run on aipu sim through triton
    Change-Id: I37020b5fad1c803a1e9284e605e569cb00d91263

--- third_party/aipu/backend/driver.py
+++ third_party/aipu/backend/driver.py
@@ -1,30 +1,19 @@
-import functools
 import os
-import sysconfig
-import hashlib
 import subprocess
-import tempfile
-from pathlib import Path
-from triton.runtime.build import _build
-from triton.runtime.cache import get_cache_manager
-from triton.runtime import _allocation
-from triton.backends.compiler import GPUTarget
-from triton.backends.driver import GPUDriver
-from triton._utils import parse_list_string
-
-current_dir = Path(__file__).resolve().parent
 import torch
-from torch.utils import cpp_extension
-module = cpp_extension.load(name="aipu",sources=[current_dir / "aipu_torch_dev.cpp"], extra_include_paths=[], extra_cflags=["-g"], verbose=True)
-torch.utils.rename_privateuse1_backend("aipu")
-torch._register_device_module("aipu", module)
-torch.utils.generate_methods_for_privateuse1_backend(for_storage=True)
-
+import numpy as np
+from pathlib import Path
+from triton.backends.compiler import GPUTarget
+from triton.backends.driver import DriverBase
 
 
 # ------------------------
 # Utils
 # ------------------------
+
+
+def load_binary(name, kernel, shared, device):
+    return None, kernel, 0, 0
 
 
 class AIPUUtils(object):
@@ -35,10 +24,8 @@ class AIPUUtils(object):
         return cls.instance
 
     def __init__(self):
-        #mod = compile_module_from_src(Path(os.path.join(dirname, "aipu_driver.c")).read_text(), "aipu_utils")
-        #self.load_binary = mod.load_binary
-        #self.get_device_properties = mod.get_device_properties
-        pass
+        self.load_binary = load_binary
+        self.get_device_properties = lambda device: {"max_shared_mem": 256 * 1024}
 
 
 # ------------------------
@@ -46,43 +33,70 @@ class AIPUUtils(object):
 # ------------------------
 
 
+def launch_aipu(input0, input1):
+    os.chdir("./tmp")
 
-def make_launcher(constants, signature, ids):
-    # create c source code to build as launcher module
-    src = ""
-    return src
+    input0.tofile("input0.bin")
+    input1.tofile("input1.bin")
+
+    command = ['aipudumper', "aipu.bin", '-i', 'input0.bin,input1.bin']
+    subprocess.run(command, capture_output=True, text=True)
+
+    command = ['aipu_simulator_x2', 'temp.cfg']
+    subprocess.run(command, capture_output=True, text=True)
+
+    sim_out = np.fromfile("./output.bin", np.float32)
+    os.chdir("..")
+
+    return sim_out
 
 
 class AIPULauncher(object):
 
     def __init__(self, src, metadata):
-        ids = {"ids_of_const_exprs": src.fn.constexprs if hasattr(src, "fn") else tuple()}
-        constants = src.constants if hasattr(src, "constants") else dict()
-        constants = {idx: value for idx, value in constants.items()}
-        signature = {idx: value for idx, value in src.signature.items()}
-        src = make_launcher(constants, signature, ids)
-        # mod = compile_module_from_src(src, "__triton_launcher")
-        # self.launch = mod.launch
+        self.launch = launch_aipu
 
     def __call__(self, gridX, gridY, gridZ, stream, function, *args):
-        # self.launch(gridX, gridY, gridZ, stream, function, global_scratch, *args)
-        # call function here
-        pass
+        # Store aipu.bin
+        aipu_bin = function
+        with open("./tmp/aipu.bin", 'wb') as file:
+            file.write(aipu_bin)
+
+        block_size = 1024
+
+        # Store inputs
+        print(len(args))
+        x, y, output, n = args[-4:]
+        x, y = x.aipu(), y.aipu()
+
+        x_np = x.cpu().numpy()
+        y_np = y.cpu().numpy()
+
+        sim_out = []
+        idx = 0
+        for i in range(gridX):
+            sim_out.append(self.launch(x_np[idx:idx + block_size], y_np[idx:idx + block_size]))
+            idx += block_size
+
+        sim_out = np.concatenate(sim_out)
+        output.copy_(torch.from_numpy(sim_out[:n]))
 
 
-class AIPUDriver(GPUDriver):
+class AIPUDriver(DriverBase):
 
     def __init__(self):
         self.utils = AIPUUtils()  # TODO: make static
         self.launcher_cls = AIPULauncher
+
+        import torch
+        self.get_current_stream = lambda x: x
+        self.get_current_device = torch.aipu.current_device
+
         super().__init__()
 
     def get_current_target(self):
-        device = 0
-        capability = self.get_device_capability(device)
-        capability = capability[0] * 10 + capability[1]
-        warp_size = 32
-        return GPUTarget("aipu", capability, warp_size)
+        warp_size = 4
+        return GPUTarget("aipu", "x2", warp_size)
 
     def get_active_torch_device(self):
         import torch
@@ -95,6 +109,27 @@ class AIPUDriver(GPUDriver):
     @staticmethod
     def is_active():
         import torch
+        from torch.utils import cpp_extension
+
+        try:
+            torch.aipu.is_available()
+        except:
+            current_dir = Path(__file__).resolve().parent
+            # extra_ldflags = [f"-L{path}" for path in os.getenv("LD_LIBRARY_PATH").split(":")]
+            extra_ldflags = ["-L/project/ai/scratch01/arozha01/Workspace/test-driver/Compass_NPU_Driver/Linux/bin/sim/release"]
+            extra_ldflags.append("-Wl,-rpath=/project/ai/scratch01/arozha01/Workspace/test-driver/Compass_NPU_Driver/Linux/bin/sim/release")
+            extra_ldflags.append("-laipudrv")
+            module = cpp_extension.load(
+                name="aipu",
+                sources=[current_dir / "aipu_torch_dev.cpp"],
+                extra_include_paths=[os.getenv("INCLUDE_PATH", "")],
+                extra_ldflags=extra_ldflags,
+                verbose=True
+            )
+
+            torch.utils.rename_privateuse1_backend("aipu")
+            torch._register_device_module("aipu", module)
+            torch.utils.generate_methods_for_privateuse1_backend(for_storage=True)
         return torch.aipu.is_available()
 
     def get_benchmarker(self):
