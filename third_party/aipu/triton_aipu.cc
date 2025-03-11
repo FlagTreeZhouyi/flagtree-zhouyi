68657bbe20bf5365a72405de3a2b83f20b4f6fad
chunying <chunying.lv@armchina.com>
2025-03-11 10:36:57 +0800

    [AIPU][cp-15696] support vectorize transform and codegen
    Change-Id: I139c6fb7f1b1dc6be7d41236a1fb29f954828033
    (cherry picked from commit 278d90ef796e3964cae2b2b56563810c5fdee7d2)

--- third_party/aipu/triton_aipu.cc
+++ third_party/aipu/triton_aipu.cc
@@ -1,22 +1,26 @@
-#include "mlir/Pass/PassManager.h"
-#include "mlir/Transforms/Passes.h"
+#include "mlir/Conversion/AffineToStandard/AffineToStandard.h"
 #include "mlir/Conversion/Passes.h"
-#include "mlir/Pass/Pass.h"
-#include "mlir/Dialect/Linalg/Passes.h"
-#include "mlir/Dialect/Bufferization/Transforms/Passes.h"
-#include "triton-shared/Conversion/TritonToLinalg/TritonToLinalg.h"
-#include "triton-shared/Conversion/TritonToLinalgExperimental/TritonToLinalgExperimental.h"
-#include "passes.h"
-#include "mlir/Dialect/Arith/Transforms/BufferizableOpInterfaceImpl.h"
-#include "mlir/Dialect/Linalg/IR/Linalg.h"
+#include "mlir/Dialect/Affine/Passes.h"
 #include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/Arith/Transforms/BufferizableOpInterfaceImpl.h"
+#include "mlir/Dialect/Bufferization/Transforms/FuncBufferizableOpInterfaceImpl.h"
+#include "mlir/Dialect/Bufferization/Transforms/Passes.h"
+#include "mlir/Dialect/Func/Extensions/AllExtensions.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/Linalg/IR/Linalg.h"
+#include "mlir/Dialect/Linalg/Passes.h"
 #include "mlir/Dialect/Linalg/Transforms/AllInterfaces.h"
+#include "mlir/Dialect/SCF/Transforms/BufferizableOpInterfaceImpl.h"
 #include "mlir/Dialect/Tensor/IR/Tensor.h"
 #include "mlir/Dialect/Tensor/Transforms/BufferizableOpInterfaceImpl.h"
-#include "mlir/Dialect/Func/IR/FuncOps.h"
-#include "mlir/Dialect/Func/Extensions/AllExtensions.h"
-#include "mlir/Dialect/Bufferization/Transforms/FuncBufferizableOpInterfaceImpl.h"
-#include "mlir/Dialect/SCF/Transforms/BufferizableOpInterfaceImpl.h"
+#include "mlir/Dialect/Vector/IR/ValueBoundsOpInterfaceImpl.h"
+#include "mlir/Dialect/Vector/IR/VectorOps.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/Pass/PassManager.h"
+#include "mlir/Transforms/Passes.h"
+#include "passes.h"
+#include "triton-shared/Conversion/TritonToLinalg/TritonToLinalg.h"
+#include "triton-shared/Conversion/TritonToLinalgExperimental/TritonToLinalgExperimental.h"
 
 #include <pybind11/pybind11.h>
 #include <pybind11/functional.h>
@@ -35,6 +39,16 @@ void init_triton_aipu_passes_convert(py::module &&m) {
   ADD_PASS_WRAPPER_0("add_affine_to_std", createLowerAffinePass);
   ADD_PASS_WRAPPER_0("add_triton_to_linalg_pipeline", triton::createTritonToLinalgExperimentalPass);
   ADD_PASS_WRAPPER_0("add_linalg_to_loops", createConvertLinalgToLoopsPass);
+  ADD_PASS_WRAPPER_0("add_linalg_to_affine_loops",
+                     createConvertLinalgToAffineLoopsPass);
+  ADD_PASS_WRAPPER_0("add_lower_affine", createLowerAffinePass);
+
+  m.def("add_affine_vectorize", [](mlir::PassManager &pm, int64_t vecsize) {
+    affine::AffineVectorizeOptions vectorize_options;
+    vectorize_options.vectorSizes.push_back(vecsize);
+    pm.addNestedPass<func::FuncOp>(
+        affine::createAffineVectorize(vectorize_options));
+  });
 }
 
 
@@ -45,9 +59,8 @@ void init_triton_aipu(py::module &&m) {
   m.def("load_dialects", [](mlir::MLIRContext &context) {
     using namespace mlir;
     DialectRegistry registry;
-    registry.insert<arith::ArithDialect,
-                    linalg::LinalgDialect,
-                    tensor::TensorDialect,
+    registry.insert<arith::ArithDialect, linalg::LinalgDialect,
+                    tensor::TensorDialect, vector::VectorDialect,
                     func::FuncDialect>();
 
     arith::registerBufferizableOpInterfaceExternalModels(registry);
