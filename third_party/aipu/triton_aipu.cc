e9951d75efa09c90fc6600134738c330e7282650
Qiang Zhang <qiang.zhang@armchina.com>
2025-04-10 11:25:49 +0800

    [AIPU] Fix Lint Clang Format Issues
    Change-Id: I1382b552467f7ed5505efaad601c00820fc45306
    (cherry picked from commit eac76cc63f0c190f72b554e467e0f3290126c303)

--- third_party/aipu/triton_aipu.cc
+++ third_party/aipu/triton_aipu.cc
@@ -22,22 +22,24 @@
 #include "triton-shared/Conversion/TritonToLinalg/TritonToLinalg.h"
 #include "triton-shared/Conversion/TritonToLinalgExperimental/TritonToLinalgExperimental.h"
 
-#include <pybind11/pybind11.h>
 #include <pybind11/functional.h>
+#include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <pybind11/stl_bind.h>
 
 namespace py = pybind11;
 
-
 void init_triton_aipu_passes_convert(py::module &&m) {
   using namespace mlir;
   ADD_PASS_WRAPPER_0("add_linalg_to_std", createConvertLinalgToStandardPass);
-  ADD_PASS_WRAPPER_0("add_one_shot_bufferize", bufferization::createOneShotBufferizePass);
-  ADD_PASS_WRAPPER_0("add_buff_to_memref", createConvertBufferizationToMemRefPass);
+  ADD_PASS_WRAPPER_0("add_one_shot_bufferize",
+                     bufferization::createOneShotBufferizePass);
+  ADD_PASS_WRAPPER_0("add_buff_to_memref",
+                     createConvertBufferizationToMemRefPass);
   ADD_PASS_WRAPPER_0("add_triton_to_linalg", triton::createTritonToLinalgPass);
   ADD_PASS_WRAPPER_0("add_affine_to_std", createLowerAffinePass);
-  ADD_PASS_WRAPPER_0("add_triton_to_linalg_pipeline", triton::createTritonToLinalgExperimentalPass);
+  ADD_PASS_WRAPPER_0("add_triton_to_linalg_pipeline",
+                     triton::createTritonToLinalgExperimentalPass);
   ADD_PASS_WRAPPER_0("add_linalg_to_loops", createConvertLinalgToLoopsPass);
   ADD_PASS_WRAPPER_0("add_linalg_to_affine_loops",
                      createConvertLinalgToAffineLoopsPass);
@@ -50,7 +52,6 @@ void init_triton_aipu_passes_convert(py::module &&m) {
         affine::createAffineVectorize(vectorize_options));
   });
 }
-
 
 void init_triton_aipu(py::module &&m) {
   auto passes = m.def_submodule("passes");
@@ -67,7 +68,7 @@ void init_triton_aipu(py::module &&m) {
     linalg::registerAllDialectInterfaceImplementations(registry);
     tensor::registerBufferizableOpInterfaceExternalModels(registry);
     bufferization::func_ext::registerBufferizableOpInterfaceExternalModels(
-      registry);
+        registry);
     func::registerAllExtensions(registry);
     scf::registerBufferizableOpInterfaceExternalModels(registry);
     context.appendDialectRegistry(registry);
